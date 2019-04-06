4d132f7d5b9707383e41575b6a6824d9b6d322ad
Wenhao Lee <wenceslauslee@gmail.com>
2019-04-05 22:28:00 -0400

    Add validation and post confirmation lambda to save user into DB

--- test/puzzle/user-manager-integ-test.js
+++ test/puzzle/user-manager-integ-test.js
@@ -29,14 +29,40 @@ describe('userManager', () => {
   });
 
   describe('updateUser', () => {
-    it('should return error message if validation fails', async () => {
-      const updatedValues = {
-        givenname: ''
-      };
-      const result = await userManager.updateUser('test-user', updatedValues);
+    const input = [
+      { 'key': 'givenname', 'value': '' },
+      { 'key': 'email', 'value': 'abcde' },
+      { 'key': 'gender', 'value': 'mfemal' },
+      { 'key': 'birthdate', 'value': '2019-02-29' },
+      { 'key': 'birthdate', 'value': '2019-02-281' }
+    ];
+    const expectedMessage = [
+      'Given name is not valid.',
+      'Email is not valid.',
+      'Gender is not valid.',
+      'Birthdate is not valid.',
+      'Birthdate is not valid.'
+    ];
 
-      assert.strictEqual(result.statusCode, 404);
-      assert.strictEqual(result.message, 'User info is not valid');
-    });
+    function testInputs(i) {
+      const updatedValues = {
+        givenname: 'Test User',
+        email: 'wenceslauslee@yahoo.com.tw',
+        gender: 'male',
+        birthdate: '1990-01-01'
+      };
+
+      it(`should return '${expectedMessage[i]}' when given wrong input`, async () => {
+        updatedValues[input[i]['key']] = input[i]['value'];
+        const result = await userManager.updateUser('test-user', updatedValues);
+
+        assert.strictEqual(result.statusCode, 404);
+        assert.strictEqual(result.message, expectedMessage[i]);
+      });
+    }
+
+    for (var i = 0; i < input.length; i++) {
+      testInputs(i);
+    }
   });
 });
